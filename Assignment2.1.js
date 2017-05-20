0f2c0679689ca182f4ba40b1d8a57d267439f7eb
f3us <raul.onnet@gmail.com>
2017-05-20 16:47:09 +0530

    Day 2-Assignment 1
    1. Declare variables using JavaScript to store your name, age,
    dateofbirth and placeofbirth. Display the values of these variables in
    console.
    2. Create an object using object literal notation that stores your name,
    age, dateofbirth and placeofbirth and display the value of each property
    in console.

---
+++ Assignment2.1.js
@@ -0,0 +1,14 @@
+var Name = "Biju";
+var Age = 29;
+var DateOfBirth = "20-Apr-1988";
+
+console.log(Name +" "+ Age +" "+ DateOfBirth);
+
+
+var MyObj = {
+  "objName":"Rahul",
+  "objAge":29,
+    "objDateOfBirth":"15-feb-1988"
+};
+
+console.log(MyObj.objName +" "+ MyObj.objAge +" "+ MyObj.objDateOfBirth);
